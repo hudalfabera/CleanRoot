24400693e02539f7b1af43dda5571580944a4036
Disperosa <disperosas@gmail.com>
2026-05-15 00:32:12 +0200

    Initial Commit

---
+++ CleanRoot/CleanRootApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanRootApp.swift
+//  CleanRoot
+//
+//  Created by Hüdalfa Bera on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanRootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
